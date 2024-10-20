b6434b795927ef2f0b6965d69a360a9a6d70ae94
7b809 <111459449+7b809@users.noreply.github.com>
2024-10-20 23:22:58 +0530

    Update getImages.js

--- netlify/functions/getImages.js
+++ netlify/functions/getImages.js
@@ -4,22 +4,44 @@ const { MongoClient } = require('mongodb');
 exports.handler = async (event, context) => {
     const mongoUrl = process.env.MONGO_URL; // Use environment variable for MongoDB URL
     const client = new MongoClient(mongoUrl);
-    
+
+    // Extract the path from the event object
+    const pathParts = event.path.split('/');
+    const index = parseInt(pathParts[pathParts.length - 1], 10); // Get the last part of the path
+
     try {
         await client.connect();
         const db = client.db('project-h');
         const collection = db.collection('api-img');
 
-        // Fetch all documents in the collection
-        const documents = await collection.find().toArray();
+        if (!isNaN(index)) {
+            // If an index is provided in the URL
+            const documents = await collection.find().toArray();
+            // Check if the index is valid
+            if (index < 0 || index >= documents.length) {
+                return {
+                    statusCode: 404,
+                    body: JSON.stringify({ error: 'Document not found' }),
+                };
+            }
+            // Remove the "_id" field from the specified document
+            const { _id, ...rest } = documents[index];
 
-        // Remove the "_id" field from each document
-        const sanitizedDocuments = documents.map(({ _id, ...rest }) => rest);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(rest), // Send the sanitized document as a response
+            };
+        } else {
+            // Fetch all documents if no index is provided
+            const documents = await collection.find().toArray();
+            // Remove the "_id" field from each document
+            const sanitizedDocuments = documents.map(({ _id, ...rest }) => rest);
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(sanitizedDocuments),
-        };
+            return {
+                statusCode: 200,
+                body: JSON.stringify(sanitizedDocuments), // Send all documents as a response
+            };
+        }
     } catch (error) {
         return {
             statusCode: 500,
